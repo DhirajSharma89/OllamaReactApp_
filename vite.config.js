5c07806508025fc89c2e486fa990f465e9b3225d
DhirajSharma89 <rhombahu@gmail.com>
2025-06-19 22:51:06 +0530

    last ones

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 5173,
+    open: true,
+    // This is the key fix to enable React Router to handle deep links like /signin
+    historyApiFallback: true,
+  },
+})
