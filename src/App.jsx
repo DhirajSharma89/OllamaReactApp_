cf46a5af07c8312b427abc0f5160931d6ab538e9
DhirajSharma89 <rhombahu@gmail.com>
2025-06-19 10:59:41 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,27 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import Contact from "./pages/Contact";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import "./App.css"; // Ensure global styles are applied
+import ImageGen from "./pages/ImageGen";
+import "./App.css";
 
 const App = () => {
   return (
-    <Router>
-      <div className="app">
-        <Header />
-        <hr className="header-divider" />
-
-        <Routes>
-          <Route path="/" element={<MainContent />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-
-        <Footer />
-      </div>
-    </Router>
+    <div className="app">
+      <Header />
+      <hr className="header-divider" />
+      <Routes>
+        <Route path="/" element={<MainContent />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/generate-image" element={<ImageGen />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 };
 
